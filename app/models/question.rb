3e81fb5a16bd1509e0718f9f0835fa73c99673f3
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-12 02:17:36 +0300

    Add length validation to Question model for title and body

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  
-  validates :title, presence: true
-  validates :body, presence: true  
+
+  validates :title, presence: true, length: { minimum: 10 }
+  validates :body,  presence: true, length: { minimum: 10 }  
 end
