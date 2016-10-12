c57554c675724ae850ff6f7a1799a49e1550c485
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-13 00:58:01 +0300

    Add controller specs for question#new

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { FactoryGirl.create(:question) }
 
+  describe 'GET #new' do
+    it 'assigns new quastion to @quastion' do
+      get :new
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders view new' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
 end
