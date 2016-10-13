0bac87d107581c0c6fe0a76e2bb43325070a20e9
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-13 04:00:38 +0300

    Add specs and controller methods for answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,22 @@
 class AnswersController < ApplicationController
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+
+    if @answer.save
+      redirect_to question_path(@answer.question)
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def answer_params
+    params.require(:answer).permit(:question_id, :body)
+  end
+
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
+require 'pp'
 
-RSpec.describe AnswersController, type: :controller do
+describe AnswersController do
+  let(:question) { create(:question) }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question.id } }
+
+    it 'assigns new answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders view new' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      let(:params) do
+        { 
+          answer:      attributes_for(:answer).merge( { question_id: question.id } ), 
+          question_id: question.id
+        }
+      end
+
+      it 'saves the answer to database' do
+        expect { post :create, params: params }.to change(Answer, :count).by(1)
+      end
+
+      # Предполагается, что после добавления ответа — редирект на страницу вопроса (со списком ответов)
+      it 'redirects to questions#show view' do
+        post :create, params: params
+        expect(response).to redirect_to question_path(question)
+      end
+    end
+
+    context 'with invalid attributes' do 
+      let(:params) do
+        { 
+          answer:      attributes_for(:invalid_answer).merge( { question_id: question.id } ), 
+          question_id: question.id
+        }
+      end
+
+      it 'does not save the answer to database' do
+        expect { post :create, params: params }.to_not change(Answer, :count)
+      end
+
+      it 'renders new view' do
+        post :create, params: params
+        expect(response).to render_template :new
+      end
+    end
+  end
 
 end
