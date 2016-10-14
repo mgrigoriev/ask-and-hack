3ea465cb2541d12fcba6206a9911ee8312eccdf0
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-14 03:23:25 +0300

    Configure rubocop

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
-  has_many :answers, dependent: :destroy
+  has_many :answers, dependent: :destroy 
+  
 
   validates :title, presence: true, length: { minimum: 10 }
   validates :body,  presence: true, length: { minimum: 10 }
