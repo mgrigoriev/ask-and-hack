59a244bd92dab93cbd15ae0044eecf7a30c5aeb1
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-14 03:39:35 +0300

    Enable cop names in rubocop output

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,5 @@
 class Question < ApplicationRecord
-  has_many :answers, dependent: :destroy 
-  
+  has_many :answers, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 10 }
   validates :body,  presence: true, length: { minimum: 10 }
