541683daceeeaad6eb3947f3d6c0520c39f3fdc7
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-17 01:17:06 +0300

    Add feature spec scenario: 'User reads question and answers'

--- spec/feature/view_question_spec.rb
+++ spec/feature/view_question_spec.rb
@@ -1,1 +1,19 @@
-# Пользователь может просматривать вопрос и ответы к нему
+require 'rails_helper'
+
+feature 'View question and answers', %q{
+  In order to find useful information
+  As User
+  I want to be able to read question and answers
+} do
+
+    scenario 'User reads question and answers' do
+      question = create(:question_with_answers)
+      
+      visit question_path question
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body      
+      question.answers.each do |answer|
+        expect(page).to have_content answer.body
+      end
+    end
+end
