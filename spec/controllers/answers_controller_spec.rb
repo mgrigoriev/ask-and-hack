19b1c8692d59e7cc581afedf46f327f1ebf1085e
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-17 23:38:09 +0300

    Add controller spec for answer: 'DELETE #destroy'

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe AnswersController do
-  let(:question) { create(:question) }
 
   describe 'POST #create' do
+    let(:question) { create(:question) }
     login_user
+
     context 'with valid attributes' do
       let(:params) do
         {
@@ -41,4 +42,38 @@ describe AnswersController do
       end
     end
   end
+
+  describe 'DELETE #destroy' do
+    login_user
+    
+    context 'is author' do
+      let(:question) { @user.questions.create(title: 'My question title', body: 'My question body') }
+      let(:answer)   { question.answers.create(body: 'My answer body', user_id: @user.id) }
+
+      before { answer }
+
+      it 'deletes answer from database' do
+        expect { delete :destroy, params: { id: answer.id } }.to change(question.answers, :count).by(-1)
+      end
+
+      it 'redirects to questions#show view' do
+        delete :destroy, params: { id: answer.id }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'is not author' do
+      let(:user)     { create(:user) } 
+      let(:question) { user.questions.create(title: 'My question title', body: 'My question body') }
+      let(:answer)   { question.answers.create(body: 'My answer body', user_id: user.id) }
+
+      before { answer }
+
+      it 'does not delete answer from database' do
+        byebug
+        expect { delete :destroy, params: { id: answer.id } }.to_not change(Answer, :count)
+      end
+    end
+  end
+
 end
