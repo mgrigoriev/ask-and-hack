4c39c8c36f90075a3d63ca17f44c9e28c632d54a
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-17 16:07:10 +0300

    Add feature spec 'Siging in'. Add devise.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   sequence :email do |n|
-    "user#{n}@test.com"
+    "user#{n}@example.com"
   end
 
   factory :user do
--- spec/feature/sign_in_spec.rb
+++ spec/feature/sign_in_spec.rb
@@ -6,20 +6,16 @@ feature 'Siging in', %q{
   I want be able to sign in
  } do
 
-  given(:user) { create(:user) }
+  given(:user)       { create(:user) }
 
   scenario "Existing user tries to sign in" do
     sign_in(user)
-
     expect(page).to have_content 'Signed in successfully.'
   end
 
   scenario 'Non-existing user try to sign in' do
-    visit new_user_session_path
-    fill_in 'Email', with: 'wrong@user.com'
-    fill_in 'Password', with: '12345'
-    click_on 'Sign in'
-
-    expect(page).to have_content 'Invalid email or password.'
+    user.email = 'wrong@example.com'
+    sign_in(user)
+    expect(page).to have_content 'Invalid Email or password'
   end
 end
