24d98d850392d2b0094efec9d5e5ffbe6c3189b9
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-17 18:32:30 +0300

    Add authentication requirement to feature spec 'Create question'

--- spec/feature/questions/create_question_spec.rb
+++ spec/feature/questions/create_question_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 feature 'Create question', %q{
   In order to get answer from community
-  As User
+  As Authenticated User
   I want to be able to ask questions
 } do
 
-  background do
-    visit questions_path  
-    click_on 'Ask question'
-  end
+  given(:user) { create(:user) }
 
   scenario 'User creates question with valid data' do
+    sign_in(user)
+    visit questions_path
+    click_on 'Ask question'
     fill_in 'Title', with: 'My question title'
     fill_in 'Description', with: 'My question body'
     click_on 'Submit'
@@ -23,10 +23,20 @@ feature 'Create question', %q{
   end
 
   scenario 'User creates question with invalid data' do
+    sign_in(user)
+    visit questions_path
+    click_on 'Ask question'
     click_on 'Submit'
 
     expect(page).to have_css '.error_explanation'
-    expect(page).to have_css '.field_with_errors'    
+    expect(page).to have_css '.field_with_errors'
   end
+
+  scenario 'Non-authenticated user tries to create question' do
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
+  end  
   
 end
