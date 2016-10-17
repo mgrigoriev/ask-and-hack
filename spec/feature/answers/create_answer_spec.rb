c4642c39f757116d2a5422554f8a71ce54a5ae5b
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-17 19:11:58 +0300

    Add authentication requirement to feature spec 'Create answer'

--- spec/feature/answers/create_answer_spec.rb
+++ spec/feature/answers/create_answer_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 # Пользователь, находясь на странице вопроса, может написать ответ на вопрос
 feature 'Create answer', %q{
   In order to help another user
-  As User
+  As Authenticated User
   I want to create the answer to the question
 } do 
 
-  background do
-    @question = create(:question)
-    visit question_path @question
-  end
+  given(:user) { create(:user) }
+
+  background { visit question_path create(:question) }
 
   scenario 'User creates the answer with valid data' do
     fill_in 'Your Answer', with: 'My answer to the question'
@@ -20,7 +19,13 @@ feature 'Create answer', %q{
 
   scenario 'User creates the answer with invalid data' do
     click_on 'Submit'
-    expect(page).to_not have_content('My answer to the question')
     expect(page).to have_css '.error_explanation'    
   end
+
+  scenario 'Non-authenticated user tries to create answer' do
+    fill_in 'Your Answer', with: 'My answer to the question'
+    click_on 'Submit'
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
+  end  
+
 end
