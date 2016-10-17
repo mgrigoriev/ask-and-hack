343b453d057232fdb083c792fef8f28f427fdbc2
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-18 00:11:44 +0300

    Add answers#destroy method. Add current user to answers#create method.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,6 +4,7 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.build(answer_params)
+    @answer.user = current_user
 
     if @answer.save
       redirect_to @question
@@ -11,6 +12,15 @@ class AnswersController < ApplicationController
       @question.reload
       render 'questions/show'
     end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    if @answer.user == current_user
+      @answer.destroy
+      flash[:notice] = 'Answer deleted successfully'
+    end
+    redirect_to question_path @answer.question
   end
 
   private
