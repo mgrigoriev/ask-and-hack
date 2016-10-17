e9c85d2d26c41550b50c39b1a91c7f08fc5a4748
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-18 01:02:19 +0300

    Require user authentication for answers#destroy action

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!, only: :create
+  before_action :authenticate_user!, only: [:create, :destroy]
   
   def create
     @question = Question.find(params[:question_id])
