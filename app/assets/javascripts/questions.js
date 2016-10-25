ee80e386ffb8bf37a1fa5956d8a4f59482a513fa
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-25 14:36:16 +0300

    Optimize views

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -8,17 +8,14 @@ function ready() {
   $('.edit-question-link').click(function(event) {
     event.preventDefault();    
     question_id = $(this).data('questionId');
-    $('.question-title-'    + question_id).hide();
-    $('.question-body-'     + question_id).hide();
-    $('.question-controls-' + question_id).hide();    
+    $('#question-content-' + question_id).hide();
     $('form#edit-question-' + question_id).show();
   });
 
   $('.edit-answer-link').click(function(event) {
     event.preventDefault();    
-    answer_id = $(this).data('answerId');
-    $('.answer-body-'     + answer_id).hide();
-    $('.answer-controls-' + answer_id).hide();    
+    answer_id = $(this).data('answerId');    
+    $('#answer-content-' + answer_id).hide();
     $('form#edit-answer-' + answer_id).show();
   });  
 }
