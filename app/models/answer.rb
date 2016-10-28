1c715c3c714182fdf7699fdc92280ba0c9bc02e9
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-29 00:59:17 +0300

    Refactor: add concern 'attachable'

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,13 @@
 class Answer < ApplicationRecord
+  include Attachable
+
   default_scope { order('best DESC, created_at') }
 
   belongs_to :question
   belongs_to :user
-  has_many :attachments, as: :attachable, dependent: :destroy
 
   validates :body, presence: true, length: { minimum: 10 }
   validates :best, uniqueness: { scope: :question_id }, if: :best?
-
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   def make_best
     transaction do
