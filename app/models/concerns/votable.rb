77341e0d7d9a4f36953e4e7dfe05641c664a6db8
Michael Grigoriev <mgrigoriev@gmail.com>
2016-10-31 03:09:01 +0300

    Refactor Question with concern Votable

---
+++ app/models/concerns/votable.rb
@@ -0,0 +1,41 @@
+module Votable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :votable
+  end
+
+  def vote_up(user)
+    if user.author_of?(self)
+      error = "You can't vote for your own post"
+    elsif has_vote_up_from?(user)
+      error = "You can't vote up the post twice"
+    else
+      cancel_vote_from(user) if has_vote_down_from?(user)
+      votes.create(user: user, value: 1)
+    end
+  end
+
+  def vote_down(user)
+    if user.author_of?(self)
+      error = "You can't vote for your own post"
+    elsif has_vote_down_from?(user)
+      error = "You can't vote up the post twice"
+    else
+      cancel_vote_from(user) if has_vote_up_from?(user)
+      votes.create(user: user, value: -1)
+    end
+  end
+
+  def cancel_vote_from(user)
+    votes.find_by(user: user).try(:destroy)
+  end
+
+  def has_vote_up_from?(user)
+    votes.find_by(user: user, value: 1).present?
+  end
+
+  def has_vote_down_from?(user)
+    votes.find_by(user: user, value: -1).present?
+  end
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,47 +1,10 @@
 class Question < ApplicationRecord
   include Attachable
+  include Votable
 
   belongs_to :user
   has_many :answers, dependent: :destroy
-  
-  # votable
-  has_many :votes, as: :votable
 
   validates :title, presence: true, length: { minimum: 10 }
   validates :body,  presence: true, length: { minimum: 10 }
-
-  # votable
-  def has_vote_up_from?(user)
-    votes.find_by(user: user, value: 1).present?
-  end
-
-  def has_vote_down_from?(user)
-    votes.find_by(user: user, value: -1).present?
-  end
-
-  def cancel_vote_from(user)
-    votes.find_by(user: user).try(:destroy)
-  end
-
-  def vote_up(user)
-    if user.author_of?(self)
-      error = "You can't vote for your own post"
-    elsif has_vote_up_from?(user)
-      error = "You can't vote up the post twice"
-    else
-      cancel_vote_from(user) if has_vote_down_from?(user)
-      votes.create(user: user, value: 1)
-    end
-  end
-
-  def vote_down(user)
-    if user.author_of?(self)
-      error = "You can't vote for your own post"
-    elsif has_vote_down_from?(user)
-      error = "You can't vote up the post twice"
-    else
-      cancel_vote_from(user) if has_vote_up_from?(user)
-      votes.create(user: user, value: -1)
-    end
-  end
 end
