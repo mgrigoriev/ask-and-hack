ed8bc78f359801f7d26e7cd32a0c53d32e586435
Michael Grigoriev <mgrigoriev@gmail.com>
2016-11-30 21:04:45 +0300

    Fix code style for hash keys

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,7 +8,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update_email
     @user = User.find(session['devise.new_user_id'])
 
-    if @user && @user.update(email: params['email'])
+    if @user && @user.update(email: params[:email])
       redirect_to questions_path
       set_flash_message(:notice, :send_instructions) if is_navigational_format?
     else
